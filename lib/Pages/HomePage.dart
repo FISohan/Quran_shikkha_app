43b13f8005a477f9f4e498e869f24361f5e4a0ba
F. I. Sohan <66239209+FISohan@users.noreply.github.com>
2022-01-20 15:32:01 +0600

    SLExpansionPanel prototype done

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: file_names
+
 import 'package:arabic_learning/Services/QuoteService.dart';
 import 'package:arabic_learning/Widgets/QuoteDisplay.dart';
+import 'package:arabic_learning/Widgets/SlExpansionPanel.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,24 +13,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool f = true;
+  double _height = 400;
+
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Scaffold(
         appBar: AppBar(
-          title: Text("App Title"),
+          title: const Text("App Title"),
         ),
-        body: Container(
-          child: Column(
-            children:  [
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: QuoteDisplay(
-          quote:  QuoteService().getQuote()
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: QuoteDisplay(quote: QuoteService().getQuote()),
             ),
-              )
-            ],
-          ),
+            Expanded(child: const SlExpansionPanel())
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => {
+            super.setState(() {
+              if (f) {
+                f = false;
+                _height = 0;
+              } else {
+                f = true;
+
+                _height = 400;
+              }
+            })
+          },
+          child: const Text("click"),
         ),
       ),
     );
